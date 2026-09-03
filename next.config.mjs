892c7daaaf2f03d1b16c474ce3e2392534dedb2c
Abhinav Kumar <iamabhinavkumar.21@gmail.com>
2026-09-03 19:55:34 +0530

    Update project configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	distDir: process.env.NODE_ENV === "development" ? ".next-dev" : ".next",
+};
 export default nextConfig;
